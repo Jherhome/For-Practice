8970471c367b2f9f752874b94d5f7a09b0ddedda
unknown <jherhome2020@gmail.com>
2026-09-02 17:04:21 +0100

    added method product

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,4 +3,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello GitHub");
     }
+
+    public static int product(int num1, int num2) {
+        return num1 * num2;
+    }
 }
